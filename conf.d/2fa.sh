382aad0872973f9c5051b1be8b9277aeaef8205b
pxtimes3 <magols@gmail.com>
2021-02-15 18:28:46 +0100

    example.sh

---
+++ conf.d/2fa.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo -e "\033[1mInstalling markshust/magento2-module-disabletwofactorauth... \033[0m"
+docker-compose exec -T phpfpm composer require markshust/magento2-module-disabletwofactorauth
+docker-compose exec -T phpfpm bin/magento module:enable MarkShust_DisableTwoFactorAuth
+
+echo -e "\033[1mDisabling 2FA... \033[0m"
+docker-compose exec -T phpfpm bin/magento config:set twofactorauth/general/enable 0
